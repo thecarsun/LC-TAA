d20287fcc3c97211f4972148421f90c89978b33b
Car Sun <thecarsun@gmail.com>
2026-02-21 10:40:54 -0800

    adding update.bat

---
+++ update.bat
@@ -0,0 +1,16 @@
+@echo off
+echo Starting LC-TAA data update...
+echo %date% %time%
+
+cd C:\Users\Car\grepos\LC-TAA
+
+echo Running scraper...
+py src/scrape_tracker.py
+
+echo Pushing to GitHub...
+git add data/processed/cases.csv data/processed/filters.json
+git commit -m "Automated data update %date%"
+git push
+
+echo Done!
+echo %date% %time%

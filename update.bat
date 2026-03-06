57e9515c01084ea5a919a4d6c0fb1c961c4a8ee3
Car Sun <thecarsun@gmail.com>
2026-03-06 09:46:33 -0800

    adding 'logging' function to daily auto update to capture errors

--- update.bat
+++ update.bat
@@ -1,16 +1,16 @@
 @echo off
-echo Starting LC-TAA data update...
-echo %date% %time%
+echo Starting LC-TAA data update... C:\Users\Car\grepos\LC-TAA\update_log.txt
+echo %date% %time% >> C:\Users\Car\grepos\LC-TAA\update_log.txt
 
 cd C:\Users\Car\grepos\LC-TAA
 
-echo Running scraper...
-py src/scrape_tracker.py
+echo Running scraper... ... >> update_log.txt
+py src/scrape_tracker.py >> update_log.txt 2>&1
 
-echo Pushing to GitHub...
-git add data/processed/cases.csv data/processed/filters.json data/processed/last_run.txt
-git commit -m "Automated data update %date%"
-git push
+echo Pushing to GitHub...  >> update_log.txt
+git add data/processed/cases.csv data/processed/filters.json data/processed/last_run.txt >> update_log.txt 2>&1
+git commit -m "Automated data update %date%" >> update_log.txt 2>&1
+git push >> update_log.txt 2>&1
 
-echo Done!
-echo %date% %time%
+echo Done! >> update_log.txt
+echo %date% %time% >> update_log.txt

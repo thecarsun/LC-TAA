c6559fec4d0fff3c7e08c4282498f537a86b11e0
Car Sun <thecarsun@gmail.com>
2026-03-06 12:25:40 -0800

    update appy error

--- update.bat
+++ update.bat
@@ -4,13 +4,13 @@ echo %date% %time% >> C:\Users\Car\grepos\LC-TAA\update_log.txt
 
 cd C:\Users\Car\grepos\LC-TAA
 
-echo Running scraper... ... >> update_log.txt
-py src/scrape_tracker.py >> update_log.txt 2>&1
+echo Running scraper... ... >> C:\Users\Car\grepos\LC-TAA\update_log.txt
+py src/scrape_tracker.py >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
 
-echo Pushing to GitHub...  >> update_log.txt
-git add data/processed/cases.csv data/processed/filters.json data/processed/last_run.txt >> update_log.txt 2>&1
-git commit -m "Automated data update %date%" >> update_log.txt 2>&1
-git push >> update_log.txt 2>&1
+echo Pushing to GitHub...  >> C:\Users\Car\grepos\LC-TAA\update_log.txt
+git add data/processed/cases.csv data/processed/filters.json data/processed/last_run.txt >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
+git commit -m "Automated data update %date%" >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
+git push >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
 
-echo Done! >> update_log.txt
-echo %date% %time% >> update_log.txt
+echo Done! >> C:\Users\Car\grepos\LC-TAA\update_log.txt
+echo %date% %time% >> C:\Users\Car\grepos\LC-TAA\update_log.txt

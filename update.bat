b4199c49371afcff123a5046c1803baecf0b83bf
Car Sun <thecarsun@gmail.com>
2026-03-18 07:49:07 -0700

    fix .bat file with correct python path

--- update.bat
+++ update.bat
@@ -7,7 +7,10 @@ cd C:\Users\Car\grepos\LC-TAA
 echo Running scraper... ... >> C:\Users\Car\grepos\LC-TAA\update_log.txt
 py -3.9 src/scrape_tracker.py >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
 
-echo Pushing to GitHub...  >> C:\Users\Car\grepos\LC-TAA\update_log.txt
+echo Running scraper... >> C:\Users\Car\grepos\LC-TAA\update_log.txt
+"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python39_64\python.exe" src/scrape_tracker.py >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
+
+echo Pushing to GitHub... >> C:\Users\Car\grepos\LC-TAA\update_log.txt
 git add data/processed/cases.csv data/processed/filters.json data/processed/last_run.txt >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
 git commit -m "Automated data update %date%" >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
 git push >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1

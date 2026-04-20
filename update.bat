5bb4af3316b7bb9afe6a9aebdadf0ad8abfd05ae
Car <thecarsun@gmail.com>
2026-04-20 00:54:44 -0700

    update

--- update.bat
+++ update.bat
@@ -5,7 +5,7 @@ echo %date% %time% >> C:\Users\Car\grepos\LC-TAA\update_log.txt
 cd C:\Users\Car\grepos\LC-TAA
 
 echo Running scraper... ... >> C:\Users\Car\grepos\LC-TAA\update_log.txt
-"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python39_64\python.exe" "C:\Users\Car\grepos\LC-TAA\src\scrape_tracker.py" >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
+"C:\Users\Car\AppData\Local\Python\pythoncore-3.14-64\python.exe" "C:\Users\Car\grepos\LC-TAA\src\scrape_tracker.py" >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
 
 echo Pushing to GitHub... >> C:\Users\Car\grepos\LC-TAA\update_log.txt
 git add data/processed/cases.csv data/processed/filters.json data/processed/last_run.txt >> C:\Users\Car\grepos\LC-TAA\update_log.txt 2>&1
